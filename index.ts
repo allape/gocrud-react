c4eed15a6b899baefb2f8f7cddbfc86121909651
as <allensnape@gmail.com>
2024-12-05 15:43:51 +0800

    refactor: refine

--- index.ts
+++ index.ts
@@ -9,6 +9,6 @@ export { default as CrudyButton } from "./src/component/CrudyButton";
 export { default as Flex } from "./src/component/Flex";
 export { default as ThemeProvider } from "./src/component/ThemeProvider";
 
-export * as cantd from "./src/config/antd.ts";
+export * as aconfig from "./src/config/antd.ts";
 export * as config from "./src/config";
 export * as ahelper from "./src/helper/antd";
