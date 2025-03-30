6d0d5e800e3f857b2c658de70b5861a10de93e31
as <allensnape@gmail.com>
2025-03-30 16:40:25 +0800

    refactor: refine

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import GoCrudVitePlugin, { i18nextPlugin } from "./vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), i18nextPlugin(), GoCrudVitePlugin()],
 });
